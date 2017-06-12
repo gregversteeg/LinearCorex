90622e6bb5e44d578c1e34b05d7f24c1ebda8b8b
Greg Ver Steeg <gversteeg@gmail.com>
2017-06-12 10:50:00 -0700

    Trying to make pip installable.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='linearcorex',
+      version='0.1',
+      description='Find latent factors that explain the most multivariate mutual information in data',
+      url='https://github.com/gregversteeg/linearcorex',
+      author='Greg Ver Steeg',
+      author_email='gversteeg@gmail.com',
+      license='AGPL-3.0',
+      packages=['linearcorex'],
+      zip_safe=False)
